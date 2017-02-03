9db1a0a4c5bae5dbff62cf21ed1cea7f732c02b7
이주성(Lee Jusung)/Project 4실/SKP <jusunglee@sk.com>
2017-02-03 17:24:43 +0900

    ActiveAndroid initialization/terminate
    Signed-off-by: 이주성(Lee Jusung)/Project 4실/SKP <jusunglee@sk.com>

---
+++ llog/src/main/java/com/skplanet/trunk/llog/MainApplication.java
@@ -0,0 +1,13 @@
+package com.skplanet.trunk.llog;
+
+/**
+ * Created by a1000990 on 2017. 2. 3..
+ */
+
+public class MainApplication extends com.activeandroid.app.Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
